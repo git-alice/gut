426a06d2366240462223e364c2988ed261eed1f1
git-alice <art.oxbow@gmail.com>
2020-06-12 15:58:06 +0300

    [Inited] setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Gut',
+      version='0.1',
+      description='Git Gut Gut',
+      url='',
+      author='Alice',
+      author_email='',
+      license='MIT',
+      packages=[''],
+      zip_safe=False)
