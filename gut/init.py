447ba4db29333bde02fa2a79565ddc7f733a561d
Alexey Zhuchkov <art.oxbow@gmail.com>
2020-06-12 14:02:34 +0000

    [Added] init func

--- gut/init.py
+++ gut/init.py
@@ -1,3 +1,9 @@
-def init():
-    print('Inited!')
+from pathlib import Path
 
+from gut import settings
+
+def init():
+  init_dirs = map(Path, ['objects', 'refs', 'heads'])
+  for init_dir in init_dirs:
+    (Path(settings.root) / init_dir).mkdir(parents=True, exist_ok=True)
+  print('Inited success.')
